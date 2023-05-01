515805e9947061099a1f8391437fe0930fe397f2
kanakhan <cjh1950@naver.com>
2023-05-01 15:58:27 +0900

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,13 @@
-print("Hello, world!")
+import math
+
+class people:
+    age = math.sin(40)
+    name = '사람'
+    def walk(self):
+        print('%s이 걷고 있습니다' %self.name)
+
+
+
+Lee = people()
+
+Lee.walk()

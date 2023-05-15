56eaebf1425e07363f5dc45ef876fd7a36c9650d
kanakhan <cjh1950@naver.com>
2023-05-15 17:46:52 +0900

    update

--- helloworld3.py
+++ helloworld3.py
@@ -1,17 +1,42 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+#컴포넌트와 클러스터링은 따로 분석해야함
 
-D = nx.DiGraph()
 
-D.add_edges_from([(1,2),(2,3),(3,2),(3,4),(3,5),
-                  (4,5),(4,6),(5,6),(6,4),(4,2)])
+G = nx.read_weighted_edgelist('edge_list.txt',
+                              delimiter = " ")
+population = {
 
-nx.draw(D, with_labels = True)
+    'Kolkata' : 4486679,
+    'Delhi' : 11007835,
+    'Mumbai' : 12442373,
+    'Guwahati' : 957352,
+    'Bangalore' : 8436675,
+    'Pune' : 3124458,
+    'Hyderabad' : 6809970,
+    'Chennai' : 4681087,
+    'Thiruvananthapuram' : 460468,
+    'Bhubaneshwar' : 837737,
+    'Varanasi' : 1198491,
+    'Surat' : 4467797,
+    'Goa' : 40017,
+    'Chandigarh' : 961587,
+}
 
-G = nx.Graph()
-G.add_nodes_from(['a','b','c','d'])
-G.add_edges_from([('a','b'),('a','c'),
-                  ('b','c'), ('c','d')])
-nx.draw(G, with_labels = True, node_color = 'blue')
+for i in list(G.nodes()):
+    G.nodes[i]['population'] = population[i]
 
+node_color = [G.degree(v) for v in G]
+node_size = [0.0005 * nx.get_node_attributes(G,'population')[v] for v in G]
+edge_width = [0.0015 * G[u][v]['weight'] for u, v in G.edges()]
+
+
+plt.axis('off')
+pos = nx.kamada_kawai_layout(G)
+nx.draw_networkx(G, pos, node_size = node_size,
+                 node_color = node_color, alpha = 0.7,
+                 with_labels = True, width = edge_width,
+                 edge_color = '.4', cmap = plt.cm.Blues)
+
+print(nx.degree_centrality(G))
 plt.show()
